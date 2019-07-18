2cf669b6300952547c0d16ca5ff444959639567d
hsiaochimai <jchmai21@gmail.com>
2019-07-18 07:04:14 -0700

    trip service not workiing

--- src/trips/trips-service.js
+++ src/trips/trips-service.js
@@ -1,10 +1,34 @@
-const TripsService={
-    getTrips(knex, id){
-       return knex.select('*')
-       .from ('trips')
-       .where('user_id', '=', id)
-       
-       
+const populateTripPlaces = async (trip, knex) => {
+  const promises = [];
+  const placeQuery = knex("places")
+    .select("*")
+    .where("trip_id", trip.id);
+
+  const placesPromise = placeQuery.then([
+    place => {
+      trip.places = place;
     }
-    }
-    module.exports = TripsService;
+  ]);
+  promises.push(placesPromise);
+
+  return Promise.all(promises);
+};
+
+const TripsService = {
+  getTrips: async (knex, id) => {
+    let trips = knex("trips");
+    trips.where("user_id", "=", id);
+    let result = [];
+    console.log(trips.toString());
+    await trips.then(data => {
+      result = data;
+    });
+    let promises = [];
+    result.forEach(trip => promises.push(populateTripPlaces(trip, knex)));
+    await Promise.all(promises);
+    return {
+      data: result
+    };
+  }
+};
+module.exports = TripsService;
